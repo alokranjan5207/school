c863b6adcc14be54bca3c82cba7a27ec950562fa
alokranjan5207 <imalok5207@gmail.com>
2024-01-14 09:54:42 +0530

    commit

--- app1/forms.py
+++ app1/forms.py
@@ -2,7 +2,18 @@ from django import forms
 from .models import Admission
 from django.core import validators
 
-class AdmissionForm(forms.Form):
+class AdmissionForm(forms.ModelForm):
     class Meta:
         model=Admission
         fields=['Student_Name','Mother_Name','Adhar_No','Contact_No','Address']
+        widgets={
+            'Student_Name':forms.TextInput(attrs={'class':'form-control'}),
+            'Mother_Name':forms.TextInput(attrs={'class':'form-control'}),
+            'Adhar_No':forms.TextInput(attrs={'class':'form-control'}),
+            'Contact_No':forms.TextInput(attrs={'class':'form-control'}),
+            'Address':forms.TextInput(attrs={'class':'form-control'}),
+
+
+
+
+        }
--- app1/views.py
+++ app1/views.py
@@ -27,14 +27,16 @@ def formdata(request):
     if request.method=='POST':
         fm=AdmissionForm(request.POST)
         if fm.is_valid():
-            nm=int(fm.changed_data['Student_Name'])
-            mn=fm.cleaned_data['Mother_Name']
-            adrn=fm.cleaned_data['Adhar_No']
-            cnt=fm.cleaned_data['Contact_No']
-            adrs=fm.cleaned_data['Address']
-            res=AdmissionForm(Student_Name=nm,Mother_Name=mn,Adhar_No=adrn,Contact_No=cnt,Address=adrs)
-            res.save()
-            fm=AdmissionForm()
+            fm.save() #sort way to save
+        # if fm.is_valid():
+        #     nm=fm.changed_data['Student_Name']
+        #     mn=fm.cleaned_data['Mother_Name']
+        #     adrn=fm.cleaned_data['Adhar_No']
+        #     cnt=fm.cleaned_data['Contact_No']
+        #     adrs=fm.cleaned_data['Address']
+        #     res=AdmissionForm(Student_Name=nm,Mother_Name=mn,Adhar_No=adrn,Contact_No=cnt,Address=adrs)
+        #     res.save()
+        #    fm=AdmissionForm()
     else:
         fm=AdmissionForm()
-    return render(request=request,template_name='1.html')
+    return render(request=request,template_name='1.html',context={'form':fm})
